78f66dc54c6142ab9e6abc1a594aca5dfdeb7642
KevinCasaWindows <kevin.alvarenga93@gmail.com>
2023-01-14 12:35:34 -0600

    clase 2

---
+++ variables.js
@@ -0,0 +1,16 @@
+var name = "kevin";
+
+var humano = new Object();
+
+humano.name = "Kevin";
+humano.lastName = "alvarenga";
+humano.age = 29;
+humano.sex = true;
+humano.modal = "";
+
+if (humano.sex == true) {
+    humano.modal = "Masculino";
+} else {
+    humano.modal = "Femenino";
+}
+console.log(humano);
