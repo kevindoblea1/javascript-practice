872b707a2c9eaee4be1e397d902fd611e872c58c
Kevin Trabajo <kevin.alvarenga93@gmail.com>
2023-01-16 15:54:37 -0600

    Se agrega contenido a piedra pape o tijera

---
+++ swtch.js
@@ -0,0 +1,30 @@
+
+var n1 = 0;
+var n2 = 0;
+import { createInterface } from "redline";
+
+let interfazCaptura = createInterface ({
+    input: process.stdin,
+    output: process.stdout
+});
+
+console.log("Calculadora en java script");
+console.log("Seleccione la opcion introduciendo el numero correspondiente: ")
+function selectMenu(){
+interfazCaptura.question("1 suma, 2 Resta, 3 Multiplicacion, 4")
+}
+selectMenu();
+
+function suma(n1,n2 ) {
+
+}
+function Resta(n1,n2 ) {
+
+}
+function multi(n1,n2 ) {
+
+}
+function division(n1,n2 ) {
+
+}
+
