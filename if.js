0cb1f833e92885bb9280db50e57391d984436836
Kevin Trabajo <kevin.alvarenga93@gmail.com>
2023-01-16 13:15:53 -0600

    se agrega .gitignore, y se activa node.js

---
+++ if.js
@@ -0,0 +1,15 @@
+if (false) {
+    console.log("Verdadero");
+}else {
+    console.log("falso");
+}
+
+var edad = 21;
+
+if (edad === 18) {
+    console.log("Puedes votar, es tu primera votacion")
+}else if (edad > 18) {
+    console.log("Puedes votar de nuevo")
+} else {
+    console.log("No puedes votar aun")
+}
