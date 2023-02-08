b0674b7257da1256fcb50a27775418acd88427dc
Kevin Trabajo <kevin.alvarenga93@gmail.com>
2023-02-08 11:58:23 -0600

    manejo de dom e innerText

--- eventos.js
+++ eventos.js
@@ -1,1 +1,12 @@
-//para detectar los eventos en la pagina web
+const h1 = document.querySelector('h1');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const parrafo = document.getElementById('resultado');
+
+
+function btnOnClick () {
+    let suma = Number(input1.value) + Number(input2.value);
+    console.log(suma);
+    parrafo.innerText = "Resultado: " + suma;
+}
