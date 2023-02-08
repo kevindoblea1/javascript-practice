ab86f407ff4699775c38c6b8bedeec51e994e0d1
Kevin Trabajo <kevin.alvarenga93@gmail.com>
2023-02-08 14:24:33 -0600

    Se finaliza acercamiento a manipulacion de dom

--- practico-javascript/eventos.js
+++ practico-javascript/eventos.js
@@ -1,11 +1,9 @@
-const h1 = document.querySelector('h1');
-const input1 = document.querySelector('#calculo1');
-const input2 = document.querySelector('#calculo2');
+const in1 = document.querySelector('#calculo1');
+const in2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
-const parrafo = document.getElementById('resultado');
+const parra = document.querySelector('#result');
 
-
-function btnOnClick () {
-    let suma = Number(input1.value) + Number(input2.value);
-    console.log(suma);
+function btnOnClick() {
+  const sum = Number(in1.value) + Number(in2.value);
+  parra.innerText = "Resultado: " + sum;
 }
