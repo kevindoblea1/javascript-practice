c3234058f3f4819f9ae8ca9572fae55cce31ae06
Kevin Trabajo <kevin.alvarenga93@gmail.com>
2023-02-08 16:41:34 -0600

    final de dia

--- practico-javascript/eventos.js
+++ practico-javascript/eventos.js
@@ -1,9 +1,13 @@
 const in1 = document.querySelector('#calculo1');
 const in2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
-const parra = document.querySelector('#result');
+const parrafo = document.querySelector('#result');
+const form = document.querySelector('#form');
+//escuchar eventos 
+btn.addEventListener('click', sumarInputValues)
 
-function btnOnClick() {
+function sumarInputValues() {
   const sum = Number(in1.value) + Number(in2.value);
-  parra.innerText = "Resultado: " + sum;
+  parrafo.innerText = "Resultado: " + sum;
 }
+
